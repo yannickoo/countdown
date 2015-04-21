4acec45a154fbfb2a290d88c4ee7ff708e911155
yannickoo <github@yannick.md>
2015-04-21 02:20:12 +0200

    adds form to configure countdown end through ui

--- js/app.js
+++ js/app.js
@@ -1,45 +1,65 @@
 $(document).ready(function() {
-  var $countdown = $('#countdown');
-  var until = new Date(2014, 4, 7);
-  var hash = location.hash.substr(1);
-  var timestamp;
-  var message = 'Countdown expired!';
+  var $countdown = $('#countdown'),
+      $form = $('form'),
+      $end = $form.children('input'),
+      until = new Date(2015, 4, 5),
+      message = 'Countdown expired!',
+      timestamp,
+      end;
 
-  if (hash) {
-    if (hash.indexOf('|')) {
-      hash = hash.split('|');
-      timestamp = hash[0];
-      message = hash[1];
-    }
-    else {
-      timestamp = hash;
+  $(window).on('hashchange', function() {
+    var hash = location.hash.substr(1);
+
+    if (hash) {
+      if (hash.indexOf('|')) {
+        hash = hash.split('|');
+        timestamp = hash[0];
+        message = hash[1];
+      }
+      else {
+        timestamp = hash;
+      }
+
+      if (parseInt(timestamp) != timestamp) {
+        $countdown.text('o_ô');
+        return;
+      }
+
+      until = new Date(timestamp * 1000);
+      end = until.toISOString().slice(0, 16);
+
+      $end.val(end);
     }
 
-    if (parseInt(timestamp) != timestamp) {
-      $countdown.text('o_ô');
-      return;
+    $countdown.countdown('destroy').countdown({
+      until: until,
+      onExpiry: function() {
+        setTimeout(function() {
+          var title = document.title;
+          setInterval(function() {
+            if (document.title == title) {
+              document.title = message;
+            }
+            else {
+              document.title = title;
+            }
+          }, 1000);
+          $countdown.text(message);
+        });
+      }
+    });
+  }).trigger('hashchange');
+
+  $form.on('submit', function(e) {
+    var time = $form.find('input[name="time"]').val(),
+        ts = new Date(time).getTime() / 1000;
+
+    if (ts) {
+      location.hash = ts;
     }
 
-    until = new Date(timestamp * 1000);
-  }
-
-  $countdown.countdown({
-    until: until,
-    onExpiry: function() {
-      setTimeout(function() {
-        var title = document.title;
-        setInterval(function() {
-          if (document.title == title) {
-            document.title = message;
-          }
-          else {
-            document.title = title;
-          }
-        }, 1000);
-        $countdown.text(message);
-      });
-    }
-  });
+    e.preventDefault();
+  })
 
   var $body = $('body');
   $(document).keydown(function(e) {
